3dd4d340400b94774c9b9bec84b4f2fb29f8a634
huda-abdulr <huda_abdulrasool@brown.edu>
2023-12-15 16:42:25 -0500

    fix: finished fixing the content in mentalHealt.tsx

--- src/components/MentalHealth.tsx
+++ src/components/MentalHealth.tsx
@@ -60,61 +60,53 @@ const accordionData = [
     ),
   },
   {
-    title: "WHAT is primary care?",
-    content: `
-    A primary care doctor takes care of all of your basic health needs and refers you to other doctors and services when needed. A good primary care doctor pays attention to your situation and surroundings so they can take care of your whole self, not just your medical conditions.
-
-    Primary care doctors are important because they take care of you regularly over a long period of time. This way, they can get to know you and your health really well. 
-    
-    Primary care services: some things your primary care doctor can do for you include:
-    Wellness visits and physicals
-    Non-urgent illness, injury, or health concerns
-    Preventative care services, like:
-    Immunizations (for example, the yearly flu shot)
-    Diagnostic and screening tests 
-    *** Primary care services are covered by Medicaid, so you don’t have to pay for them.
-    Primary care doctors can be different kinds of doctors. For example, they may be called family medicine doctors or internal medicine doctors. Pediatricians are primary care doctors for children and some OB/GYN doctors can be primary care doctors for women.
-    `,
+    title: "WHEN should I seek mental health care?",
+    content: (
+      <>
+        If you know you’re struggling with your mental health or substance use,{" "}
+        <b>
+          get connected to care as soon as you can, even if you aren’t feeling
+          bad.
+        </b>{" "}
+        <span style={{ color: "red" }}>
+          Seek help immediately if you or someone you know is in extreme
+          distress or in danger of hurting themselves or others.
+        </span>
+      </>
+    ),
   },
   {
-    title: "WHERE can I find a primary care doctor?",
-    content: `
-    You can find your primary care doctor by calling the “Primary Care Physician” on your United or Neighborhood insurance card. If you cannot find it or need a new primary care doctor, call your insurance company who can help you find one in your area (phone numbers on pg. 5)
-    Community heath centers (pg. 27) may also have primary care doctors.
-    
-    
-    `,
-  },
-  {
-    title: "WHEN should I see my primary care doctor?",
-    content: `
-    You should have a wellness visit, also known as a check-up, every 6-12 months. Make sure to talk about this with your doctor.
-    Also, make an appointment to see your primary care doctor if you have a health concern and would like to see a doctor.
-    
-    
-    `,
-  },
-  {
-    title: "TIPS & TRICKS",
-    content: `
-    In your cell phone, planner, or anywhere else that you keep important information:
-    Save your primary care doctor’s office number. You should be able to quickly and easily call to make appointments If you have any health concern.
-    Write down the date, time, and location of your next appointment. These can be made really far In the future and can be easy to forget. If you will miss an appointment, let the doctor’s office know as soon as possible, or at least 24 hours before the appointment. 
-    It may take time (several weeks or months) to get an appointment with your primary care doctor since they can be very busy. However, once you are a regular patient, you should be able to get appointments much more quickly. 
-    It is important to find a primary care doctor that you feel comfortable with. If you are seeing someone you don’t like, make sure to try others until you find the right fit.
-    A good primary care doctor should:
-    Answer your questions clearly and honestly
-    Treat you with respect
-    Take time to understand you and your situation
-    Before you see your doctor, it can be helpful to think of things you want to talk about with them. There is room to write these down on the next page.
-    `,
-  },
-  {
-    title: "WHAT does a primary care visit look like?",
-    content: `
-      Aliquam vel libero et tortor sagittis condimentum. Nam id varius turpis, id pharetra eros. Mauris purus tortor, mattis quis eros in, molestie pharetra lorem. Morbi viverra urna purus, nec ornare purus aliquet et. Curabitur tempus nulla id leo eleifend, sit amet lobortis libero interdum. Proin nulla neque, imperdiet nec metus in, volutpat accumsan sem. Curabitur imperdiet et turpis at condimentum. Nunc nec quam fringilla, porta elit nec, pellentesque ligula.
-
-    `,
+    title: "WHERE can I go for mental health care",
+    content: (
+      <>
+        <b>Talk to your primary care doctor </b>about seeking mental health care
+        services. If you feel uncomfortable talking to your primary care doctor,
+        or you don’t have a primary care doctor, some helpful resources include:
+        <ul>
+          <li>
+            <b>Mental (behavioral) health centers</b>, also called “one-stop
+            shops” that provide group support, care management, and counseling.
+            They also provide primary care doctors, pharmacies, and classes,
+            like life skills training and art/music therapy. For more
+            information about where to find behavioral health care, see page
+            ___.
+          </li>
+          <li>
+            <b>
+              Call the crisis hotline at{" "}
+              <span style={{ color: "red" }}>1-800-283-TALK</span> (8255)
+            </b>{" "}
+            if you or someone you know is in danger of hurting themselves or
+            others, or you just want someone to talk to.
+          </li>
+          <li>
+            <b>Emergency rooms</b> at hospitals can also provide behavioral
+            health care in emergency situations.{" "}
+            <b style={{ color: "red" }}>Call 911 if you are in an emergency.</b>
+          </li>
+        </ul>
+      </>
+    ),
   },
 ];
 
