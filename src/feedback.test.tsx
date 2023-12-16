6dbf8f19a4121550fa853927a2429c3d3937f4c8
Sarah Comlan <119999485+sarahcoml@users.noreply.github.com>
2023-12-16 03:42:28 -0500

    testing

---
+++ src/feedback.test.tsx
@@ -0,0 +1,29 @@
+import { test, expect } from '@playwright/test';
+
+
+test('Feedback form interaction', async ({ page }) => {
+  // Navigate to your application
+  await page.goto('http://localhost:3000/');
+
+  // Interact with the feedback form
+  await page.fill('#input-text', ''); // Providing an empty feedback to trigger validation error
+  await page.click('input[type="submit"]');
+
+  // Wait for the error message to appear
+  await page.waitForSelector('.error-message', { timeout: 5000 });
+
+  // Check if the error message is displayed
+  const errorMessage = await page.textContent('.error-message');
+  expect(errorMessage).toBe('Feedback cannot be empty');
+
+  // Interact with the feedback form with valid input
+  await page.fill('#input-text', 'This is a test feedback');
+  await page.click('input[type="submit"]');
+
+  // Wait for the feedback submission (assuming there's some visual indicator)
+  await page.waitForSelector('.submission-success', { timeout: 5000 });
+
+  // Check if the submission was successful
+  const successMessage = await page.textContent('.submission-success');
+  expect(successMessage).toBe('Feedback submitted successfully');
+});
