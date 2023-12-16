972a3b2a2b43d036c5c06bbce5c6c234f3b4d889
Sarah Comlan <119999485+sarahcoml@users.noreply.github.com>
2023-12-16 03:12:51 -0500

    feedback error management

--- src/components/feedbackBox.tsx
+++ src/components/feedbackBox.tsx
@@ -1,12 +1,34 @@
 import React, { useState } from "react";
-import "../styles/subPage.scss";
 import "../styles/feedbackBox.scss";
 
 const Feedback = () => {
+  const [feedback, setFeedback] = useState("");
+  const [isError, setIsError] = useState(false);
+
+  const handleInputChange = (e) => {
+    setFeedback(e.target.value);
+    // Reset error state when user starts typing
+    setIsError(false);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Validate input before submitting
+    if (feedback.trim() === "") {
+      // Show error if textarea is empty
+      setIsError(true);
+    } else {
+      // TODO: Implement feedback submission logic
+      console.log("Submitting feedback:", feedback);
+      // Reset feedback state after submission
+      setFeedback("");
+    }
+  };
+
   return (
     <div id="feedback-box">
       <h2>Feedback</h2>
-      <form id="feedback-form" action="/submitfeedback" method="post">
+      <form id="feedback-form" onSubmit={handleSubmit}>
         {/* TODO: implement feedback endpoint */}
         <textarea
           id="input-text"
@@ -14,7 +36,11 @@ const Feedback = () => {
           rows={4}
           cols={50}
           placeholder="Enter your feedback here..."
+          value={feedback}
+          onChange={handleInputChange}
+          className={isError ? "error" : ""}
         ></textarea>
+        {isError && <p className="error-message">Feedback cannot be empty</p>}
         <input type="submit" value="Submit"></input>
       </form>
     </div>
